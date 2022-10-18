eae2eb43f78d363baf1f599289e3b13c8018f7c6
Kale Leach <kleach1212@gmail.com>
2022-10-17 18:40:04 -0700

    Commit 4: Working on singin/out

--- client/src/SignIn/Signin.js
+++ client/src/SignIn/Signin.js
@@ -3,39 +3,41 @@ import Typed from "react-typed";
 
 function Signin() {
   return (
-    <div id="signin">
-      <Typed
-        strings={[
-          "Welcome back!",
-          "Are you excited to start bidding?",
-          "Who are you again?",
-        ]}
-        typeSpeed={40}
-      />
-      <br />
+    <div id ="signintop">
+      <button className="exit"> Exit</button>
+      <div id="login">
+        <div className="exit">
+        </div>
+        <div className="container">
+          <Typed
+            strings={[
+              "Welcome back!",
+              "Are you ready to start bidding?",
+              "Who are you again?",
+            ]}
+            typeSpeed={40}
+            className="welcome"
+          />
 
-      <Typed
-        strings={[
-          "Enter username"
-        ]}
-        typeSpeed={40}
-        backSpeed={50}
-        attr="placeholder"
-        loop
-      >
-        <input type="text" />
-      </Typed>
-      <Typed
-        strings={[
-          "Enter Password"
-        ]}
-        typeSpeed={40}
-        backSpeed={50}
-        attr="placeholder"
-        loop
-      >
-        <input type="text" />
-      </Typed>
+          <Typed
+            strings={["Enter username"]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+          >
+            <input type="text" />
+          </Typed>
+          <Typed
+            strings={["Enter Password"]}
+            typeSpeed={40}
+            backSpeed={50}
+            attr="placeholder"
+          >
+            <input type="text" />
+          </Typed>
+        </div>
+        <button> Sign in</button>
+      </div>
     </div>
   );
 }
