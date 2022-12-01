f37060fe7bea519eeea2943a7a187cf8d26c7004
Kale Leach <kleach1212@gmail.com>
2022-11-30 19:56:35 -0800

    Commit 24: Fix the refresh issue

--- client/src/Profile/Banner/Banner.js
+++ client/src/Profile/Banner/Banner.js
@@ -1,10 +1,15 @@
-import './Banner.css'
+import "./Banner.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
 function Banner({ user }) {
   // I need to fix edge case when user refreshes page we lose all user information. I need the page to be able to refresh and keep all the data
   return (
-    <div id = "profile_banner">
-      <img src= {user.profile_banner} />
+    <div id="profile_banner">
+      <div id = "edit_banner">
+        <FontAwesomeIcon icon={faPenToSquare} />
+      </div>
+      <img src={user.profile_banner} />
     </div>
   );
 }
--- client/src/Profile/Profile.js
+++ client/src/Profile/Profile.js
@@ -1,7 +1,9 @@
 import ProfileNavbar from "./ProfileNav/ProfileNavbar"
 import Banner from "./Banner/Banner"
+import { useEffect, useState } from "react";
 
 function Profile({user}){
+
   return(
     <div>
       <ProfileNavbar/>
