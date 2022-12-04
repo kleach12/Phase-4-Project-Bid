24b4998a0f9c8d9fa2166c95c77cae7b604b71f7
Kale Leach <kleach1212@gmail.com>
2022-12-04 11:34:19 -0800

    Commit 26: Adding edit mode for banner

--- client/src/Profile/Banner/Banner.js
+++ client/src/Profile/Banner/Banner.js
@@ -1,13 +1,50 @@
 import "./Banner.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
 
 function Banner({ user }) {
-  // I need to fix edge case when user refreshes page we lose all user information. I need the page to be able to refresh and keep all the data
+  const [editMode, setEditMode] = useState(false)
+  const [newBanner,  setNewBanner] = useState(null)
+
+  function editBanner(){
+    setEditMode(!editMode)
+    console.log(editMode)
+  }
+
+  function handleNewBanner(e){
+    setNewBanner(e.target.value)
+    console.log(newBanner)
+  }
+
+  function handleBannerUpdate(e){
+    e.preventDefault();
+    const formData = {
+      profile_banner: newBanner
+    }
+    fetch(`/users/${user.id}`)
+  }
+
+  if(editMode){
+    return (
+      <div id="profile_banner">
+        <div id = "edit_banner">
+          <FontAwesomeIcon icon={faPenToSquare} onClick = {editBanner} />
+        </div>
+        <img src={user.profile_banner} />
+        <h3> New Profile Url</h3>
+        <form>
+        <input type = 'text' onChange={handleNewBanner}/> 
+        <button type="submit" onClick={handleBannerUpdate}> Submit</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div id="profile_banner">
       <div id = "edit_banner">
-        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faPenToSquare} onClick = {editBanner} />
       </div>
       <img src={user.profile_banner} />
     </div>
