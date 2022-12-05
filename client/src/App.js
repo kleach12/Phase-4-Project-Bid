4f7490dbec29bf6d1f877ef7d0f081a110cc255d
Kale Leach <kleach1212@gmail.com>
2022-12-05 13:10:00 -0800

    Commit 27: Added banner update feat

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function App() {
         />
         <Route
           path="/profile"
-          element={<Profile user = {user} />}
+          element={<Profile user={user} setUser={setUser} />}
         />
         <Route
           path="/"
--- client/src/Profile/Banner/Banner.js
+++ client/src/Profile/Banner/Banner.js
@@ -3,48 +3,63 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { useState } from "react";
 
-function Banner({ user }) {
-  const [editMode, setEditMode] = useState(false)
-  const [newBanner,  setNewBanner] = useState(null)
+function Banner({ user, setUser }) {
+  const [editMode, setEditMode] = useState(false);
+  const [newBanner, setNewBanner] = useState(null);
 
-  function editBanner(){
-    setEditMode(!editMode)
-    console.log(editMode)
+  function editBanner() {
+    setEditMode(!editMode);
+    console.log(editMode);
   }
 
-  function handleNewBanner(e){
-    setNewBanner(e.target.value)
-    console.log(newBanner)
+  function handleNewBanner(e) {
+    setNewBanner(e.target.value);
+    console.log(newBanner);
   }
 
-  function handleBannerUpdate(e){
+  function handleBannerUpdate(e) {
     e.preventDefault();
     const formData = {
-      profile_banner: newBanner
-    }
-    fetch(`/users/${user.id}`)
+      profile_banner: newBanner,
+    };
+
+    fetch(`/users/${user.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data);
+      });
   }
 
-  if(editMode){
+  if (editMode) {
     return (
       <div id="profile_banner">
-        <div id = "edit_banner">
-          <FontAwesomeIcon icon={faPenToSquare} onClick = {editBanner} />
+        <div id="edit_banner">
+          <FontAwesomeIcon icon={faPenToSquare} onClick={editBanner} />
         </div>
         <img src={user.profile_banner} />
         <h3> New Profile Url</h3>
         <form>
-        <input type = 'text' onChange={handleNewBanner}/> 
-        <button type="submit" onClick={handleBannerUpdate}> Submit</button>
+          <input type="text" onChange={handleNewBanner} />
+          <button type="submit" onClick={handleBannerUpdate}>
+            {" "}
+            Submit
+          </button>
         </form>
       </div>
-    )
+    );
   }
 
   return (
     <div id="profile_banner">
-      <div id = "edit_banner">
-        <FontAwesomeIcon icon={faPenToSquare} onClick = {editBanner} />
+      <div id="edit_banner">
+        <FontAwesomeIcon icon={faPenToSquare} onClick={editBanner} />
       </div>
       <img src={user.profile_banner} />
     </div>
